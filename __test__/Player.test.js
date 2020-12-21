052d9a29d750d74f9b016645a9d5057a82235216
JRJ <jjsocc171@msn.com>
2020-12-21 15:11:46 -0600

    player potions adding to inventory and consuming potions for player

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -64,3 +64,32 @@ test("subtrack from players health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test("gets player's attck value", () => {
+    const player = new Player ('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the invetory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('use a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
+
